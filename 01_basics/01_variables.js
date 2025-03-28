b19b9cb7352983bd7f0a0a7b60f159adc0719ba2
deep <deepsurti23@gmail.com>
2025-03-28 12:42:11 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = "12345"
+let accEmail = "deepsurti@yahoo.com"
+var accPassword = "12345"
+accCity = "Ahmedabad"
+let accState
+// accountId = 2
+accEmail = "abc@gmail.com"
+accPassword = "1234565445"
+accCity = "Surat"
+
+console.log(accountId);
+console.table([accountId,accEmail,accPassword,accCity,accState])
+
+/*Prefer not to use var
+ Beacause of isssue in block scopeand functional scope
+*/
